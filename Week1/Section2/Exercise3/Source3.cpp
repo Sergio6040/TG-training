d50cb028061418a1cadf81a0ac527dc290c029c7
sancho6040 <sancho6040@hotmail.com>
2022-07-15 23:04:21 -0500

    Done 3 & 4

---
+++ Week1/Section2/Exercise3/Source3.cpp
@@ -0,0 +1,27 @@
+/*
+Exercise 3 - Positive, Negative, or Zero
+Write a program that will ask the user for a number and determine if it is a positive number, a negative number, or zero.
+
+Example output
+Please enter a number: 5
+
+Your number is positive!
+*/
+#include <iostream>
+
+int main() {
+	int value;
+
+	std::cout << "Please enter a number: ";
+	std::cin >> value;
+
+	if (value == 0) {
+		std::cout << "Your number is Zero" << std::endl;
+	}
+	else if (value > 0) {
+		std::cout << "Your number is Positive" << std::endl;
+	}
+	else if (value < 0) {
+		std::cout << "Your number is Negative" << std::endl;
+	}
+}
