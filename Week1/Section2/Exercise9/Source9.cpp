49780d426c4cadc030a8c3e0fd99c7586cc5801d
sancho6040 <sancho6040@hotmail.com>
2022-07-18 12:33:14 -0500

    Done exercise 9

---
+++ Week1/Section2/Exercise9/Source9.cpp
@@ -0,0 +1,22 @@
+/*
+* Exercise 9 - Lowercase Vowel
+Write a program that will receive an alphabetical character from the user. Then, tell them if the character is a lowercase vowel. Make sure to do this using a single if statement.
+
+Example output
+Hello! Please enter an alphabetical character: c
+Your character is not a lowercase vowel.
+*/
+
+#include <iostream>
+
+int main() {
+	char letter;
+	std::cout << "Hello! Please enter an alphabetical character: ";
+	std::cin >> letter;
+
+	if (islower(letter) && (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')) {
+		std::cout << "Your character is a lowercase vowel! :)" << std::endl;
+	}else{
+		std::cout << "Your character is not a lowercase vowel! :(" << std::endl;
+	}
+}
