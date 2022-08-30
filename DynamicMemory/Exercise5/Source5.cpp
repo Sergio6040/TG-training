28a0e01c3366a47a0211f3073d31432d5511c6b4
Sergio6040 <sancho6040@hotmail.com>
2022-08-30 16:43:35 -0500

    Done Exercise 7

--- DynamicMemory/Exercise5/Source5.cpp
+++ DynamicMemory/Exercise5/Source5.cpp
@@ -2,10 +2,10 @@
 * Exercise 5 - Dynamic Allocation for Array Sizes
 * Create a Student class that contains a char array of size 5 for a name and an int for an age.
 *
-* Ask the user to enter how many students they want in a class. 
-* Then, initialize an array of students in the heap of exactly the size they asked for. 
+* Ask the user to enter how many students they want in a class.
+* Then, initialize an array of students in the heap of exactly the size they asked for.
 * You can now probably tell how dynamic allocation is useful.
-* 
+*
 * Ask the user for a name and an age for each student and then print them all to the screen. Note
 * that you won’t be able to use names longer than 4 characters.
 * Remember to free any allocated memory.
@@ -30,6 +30,13 @@ void ShowStudents(const int ArraySize)
 
 int main()
 {
+
+	Student* TestStudent = new Student();
+	TestStudent->SetName("Joe");
+	TestStudent->SetAge(8);
+
+	delete TestStudent;
+
 	std::cout << "How many students do you want in your class? " << std::endl;
 	int ArraySize;
 	std::cin >> ArraySize;
@@ -71,8 +78,15 @@ int main()
 		}
 		ShowStudents(ArraySize);
 
-		std::cout << "\nmemcpy_s is the most efficient, easy and readable way to copy an array, instead of using an for loop" << std::endl;
 	}
+	//Quiz #6
+	std::cout << "\nmemcpy_s is the most efficient, easy and readable way to copy an array, instead of using an for loop" << std::endl;
+
+	//Quizz #7
+	std::cout << "\nWhy deleting the student object does not delete the name’s char array?" << std::endl;
+	std::cout << "R: What happened when using the delete keyword, is that the memory space where the object was saved is now free, but its variable values are still there as garbage, they are meant to be replaced by the new entering values :)" << std::endl;
+
+
 
 	delete[] StudentsList;
 }
--- DynamicMemory/Exercise5/Student.cpp
+++ DynamicMemory/Exercise5/Student.cpp
@@ -2,23 +2,6 @@
 #include <string.h>
 Student::Student() = default;
 
-/*
-Student::Student(std::string& InName, int InAge)
-{
-	if (InName.empty())
-	{
-		InName = "Joe_";
-	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		StudentName[i] = InName[i];
-	}
-
-	StudentAge = InAge;
-
-}*/
-
 std::string Student::GetName() const
 {
 	return StudentName;
