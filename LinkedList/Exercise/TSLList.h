dfea537040e0b5fe9f224c19f287a1273e7c221e
Sergio6040 <sancho6040@hotmail.com>
2022-09-13 15:39:31 -0500

    Done Part 2

--- LinkedList/Exercise/TSLList.h
+++ LinkedList/Exercise/TSLList.h
@@ -20,9 +20,53 @@ public:
 
 	TSLList() : Head(nullptr), Tail(nullptr), ListSize(0) {};
 
-	TSLList()
-	{
+	//-------------------------------------------------------------------------------------
 
+	Type& operator[] (const int Index)
+	{
+		FNode* Current = Head;
+		for (int i = 0; i < Index; i++)
+		{
+			Current = Current->Next;
+		}
+		return Current->Element;
 	}
 	
+	//-------------------------------------------------------------------------------------
+
+	const Type& operator[] (const int Index) const
+	{
+		FNode* Current = Head;
+		for (int i = 0; i < Index; i++)
+		{
+			Current = Current->Next;
+		}
+		return Current->Element;
+	}
+
+	//-------------------------------------------------------------------------------------
+
+	Type& Head()
+	{
+		return Head->Element;
+	}
+
+	const Type& Head() const
+	{
+		return Head->Element;
+	}
+
+	//-------------------------------------------------------------------------------------
+	
+	void Tail()
+	{
+		return Tail->Element;
+	}
+	
+	//-------------------------------------------------------------------------------------
+	
+	const void Tail() const
+	{
+		return Tail->Element;
+	}
 };
